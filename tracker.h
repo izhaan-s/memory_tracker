bc8dedb9175c7138e4a9c61e0e297fc28699f7c0
izhaan-s <izhaansalam@gmail.com>
2025-12-14 22:17:53 +0000

    Updated allocation struct, now includes macro to take FILE and LINE

--- tracker.h
+++ tracker.h
@@ -3,15 +3,30 @@
 
 #include <stddef.h>
 
+typedef enum {MT_LIVE, MT_FREED} State;
+
+// append-only
 typedef struct{
+    size_t id; // unique allocation instance
+
     void* ptr;
     size_t size;
+
+    const char* alloc_file;
+    int alloc_line;
+    const char* free_file;
+    int free_line;
+
+    State state;
 } Allocation;
 
-void* mt_malloc(size_t size);
+// actual implementation
+void* mt_malloc_impl(size_t size, const char* file, int line);
+void mt_free_impl(void* ptr, const char* file, int line);
 
-void mt_free(void* ptr);
-
-void mt_print_stats();
+// what users call 
+#define mt_malloc(size) mt_malloc_impl((size), __FILE__, __LINE__)
+#define mt_free(ptr) mt_free_impl((ptr), __FILE__, __LINE__)
+void mt_print_stats(void);
 
 #endif
